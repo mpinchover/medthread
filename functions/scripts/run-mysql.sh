2369844ed04f955669501b2036d4ead182a1c3dd
matt <mpinchover@gmail.com>
2023-01-31 18:46:17 -0500

    added in mysql scripts

---
+++ functions/scripts/run-mysql.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# check to see if the container with the name exists.
+existingMySQLContainer=$(docker ps -a -q -f name="local-medthread-mysql-db")
+if [ -z "$existingMySQLContainer" ]
+then
+    # conatiner has been found so tear it down for a clean database
+    echo "Found existing local medthread db " $existingMySQLContainer
+    docker run --name local-medthread-mysql-db -d -p 3308:3306 -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=root mysql:8 
+    sleep 20
+    existingMySQLContainer=$(docker ps -a -q -f name="local-medthread-mysql-db")
+fi
+
+
+docker cp setup_medthread_db.sql $existingMySQLContainer:/setup_medthread_db.sql
+# run the docker container
+
+docker exec -it $existingMySQLContainer bash -c "mysql -u root -proot < setup_medthread_db.sql" 
+
+# mysql -uroot -proot -h 127.0.0.1 -P 3308 < setup_medthread_db.sql
---
+++ functions/scripts/setup_medthread_db.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS medthread_dev;
+CREATE DATABASE medthread_dev
