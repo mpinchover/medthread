ec95d490e7f785d29704e96179ded0cb093187b3
matt <mpinchover@gmail.com>
2023-02-01 08:43:11 -0500

    added in mysql table

--- functions/scripts/setup_medthread_db.sql
+++ functions/scripts/setup_medthread_db.sql
@@ -1,2 +1,32 @@
 DROP DATABASE IF EXISTS medthread_dev;
-CREATE DATABASE medthread_dev
+CREATE DATABASE medthread_dev;
+USE medthread_dev;
+
+CREATE TABLE authorizedCareProviderLink (
+    careProviderUuid VARCHAR(36),
+    patientUuid VARCHAR(36),
+    uuid VARCHAR(36) NOT NULL UNIQUE
+);
+
+CREATE TABLE medicationRequest (
+    uuid VARCHAR(36) NOT NULL UNIQUE,
+    authoredOn DATETIME,
+    code VARCHAR(36),
+    codeDisplay TEXT,
+    source VARCHAR(36),
+    medStatus VARCHAR(36),
+    intent VARCHAR(36),
+    requesterIdentifier VARCHAR(36),
+    requester TEXT,
+    dosageInstructionText TEXT,
+    dosageInstructionRoute TEXT,
+    doseAndRateQuantityValue INT,
+    doseAndRateQuantityUnit VARCHAR(36),
+    fhirReference VARCHAR(36),
+    insuranceProviderUuid VARCHAR(36),
+    userUid VARCHAR(36),
+    resourceType TINYTEXT,
+    createdAt DATETIME,
+    updatedAt DATETIME,
+    deletedAt DATETIME
+);
